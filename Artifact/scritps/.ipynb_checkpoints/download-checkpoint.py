882fa05b7e24de543f1b8af1f7ab0f9fef66b1dc
Stanley Akor <stanleyakor@u.boisestate.edu>
2023-06-30 21:03:24 -0600

    cleaning up

--- Artifact/scritps/.ipynb_checkpoints/download-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-def download_data(args):
-    try:
-        url, filename = args
-        urllib.request.urlretrieve(url, filename)
-        return True
-    except Exception as e:
-        return str(e)
